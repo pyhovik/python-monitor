5576d86ed4b154dc271f73775e93c1fb06474ab6
Литвинов Дмитрий <dmitriy.litvinov@eltex.loc>
2023-02-10 15:43:44 +0700

    create inventor.py

---
+++ app/inventor.py
@@ -0,0 +1,15 @@
+"""
+Модуль, предоставляющий инструменты для сбора информации о целевом хосте:
+информация о системе, окружении и проч.
+"""
+
+import os
+
+class Inventor:
+    def __init__(self): pass
+    def getOsInfo(self): pass
+    def getCpuInfi(self): pass
+    def getMemInfo(self): pass
+    def getMotherboardInfo(self): pass
+    
+        
