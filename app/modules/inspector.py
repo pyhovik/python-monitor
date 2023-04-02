ee6712ecda7d65632090cdda85001de576a6f73b
Dmitrii Litvinov <dilumik@mail.ru>
2023-04-02 22:18:02 +0700

    fix inspector, add test

--- app/modules/inspector.py
+++ app/modules/inspector.py
@@ -3,12 +3,11 @@
 информация о системе, окружении и проч.
 """
 
-import json
 import os
 import psutil
 import cpuinfo
 import platform
-from subprocess import check_output, CalledProcessError
+from subprocess import check_output
 
 
 class Inspector:
@@ -27,8 +26,9 @@ class Inspector:
 
     def get_os_info(self):
         """Метод, собирающий инф-цию о ОС."""
-        self.os_hostname = os.uname().nodename
-        self.os_release = check_output(
+        self.os = {}
+        self.os['hostname'] = os.uname().nodename
+        self.os['release'] = check_output(
             'lsb_release -d', 
             encoding='utf-8', 
             shell=True
@@ -36,60 +36,43 @@ class Inspector:
     
     def get_cpu_info(self):
         """Собирает инф-цию о ЦПУ."""
-        self.cpu_model = cpuinfo.get_cpu_info()['brand_raw']
-        self.cpu_ph_core = psutil.cpu_count(logical=False)
-        self.cpu_lg_core = psutil.cpu_count()
+        self.cpu = {}
+        self.cpu['model'] = cpuinfo.get_cpu_info()['brand_raw']
+        self.cpu['ph_core'] = psutil.cpu_count(logical=False)
+        self.cpu['lg_core'] = psutil.cpu_count()
     
     def get_mem_info(self):
         """Собирает инф-цию о ОЗУ и SWAP."""
         Gb_sufix = 1024 ** 3
+        self.mem = {}
         ram_total_Gb = psutil.virtual_memory().total / Gb_sufix
         swap_total_Gb = psutil.swap_memory().total / Gb_sufix
-        self.mem_ram_total = f"{ram_total_Gb:.1f}Gb"
-        self.mem_swap_total = f"{swap_total_Gb:.1f}Gb"                              
+        self.mem['ram_total'] = f"{ram_total_Gb:.1f}Gb"
+        self.mem['swap_total'] = f"{swap_total_Gb:.1f}Gb"                              
     
     def get_motherboard_info(self):
         """Собирает инф-2ию о мат. плате"""
-        self.mb_name = check_output(
+        self.mb = {}
+        self.mb['name'] = check_output(
             "cat /sys/devices/virtual/dmi/id/board_name", 
             encoding='utf-8', 
             shell=True
             ).rstrip()
-        self.mb_vendor = check_output(
+        self.mb['vendor'] = check_output(
             "cat /sys/devices/virtual/dmi/id/board_vendor", 
             encoding='utf-8', 
             shell=True
             ).rstrip()
     
     def get_disk_info(self):
-        """Собирает инф-2ию о дисках."""
+        """Собирает инф-цию о дисках."""
         out = check_output(
-            "lsblk -J", 
+            "lsscsi -s | awk '{print $4,$5,$7,$8}'", 
             encoding='utf-8', 
             shell=True
             )
-        self.disks = []
-        jout = json.loads(out)
-        # в цикле проходимся по выводу и "собираем" диски
-        for device in jout['blockdevices']:
-            if device['type'] == 'disk':
-                self.disks.append(dict(
-                        blockdev = device['name'],
-                        size = device['size']
-                    )
-                )
-        # определить модель для каждого диска
-        try:
-            for disk in self.disks:
-                model = check_output(
-                f"sudo fdisk -l /dev/{disk['blockdev']} 2>&1 | grep model", 
-                encoding='utf-8', 
-                shell=True
-                ).split(':')[1].strip()
-                disk['model'] = model
-        except CalledProcessError:
-            print('Cannot get disk model - Permission denied.')
-        
+        self.disks = out.split('\n')[:-1]
+  
     def show_params(self):
         """Печатает все доступные параметры в консоль."""
         for attr in self.__dict__.keys():
@@ -98,23 +81,10 @@ class Inspector:
     def export_to_dict(self):
         """Метод, возвразающий все параметры системы в JSON-объекте."""
         params = {
-            'OS': {
-                'hosntame': self.os_hostname,
-                'release': self.os_release
-            },
-            'CPU': {
-                'core_physical': self.cpu_ph_core,
-                'core_logical': self.cpu_lg_core,
-                'model': self.cpu_model
-            },
-            'Memory': {
-              'ram_total': self.mem_ram_total,
-              'swap_total': self.mem_swap_total  
-            },
-            'Motherboard': {
-                'name': self.mb_name,
-                'vendor': self.mb_vendor
-            },
+            'OS': self.os,
+            'Motherboar': self.mb,
+            'CPU': self.cpu,
+            'Memory': self.mem,
             'Disks': self.disks
         }
         return params
@@ -123,3 +93,4 @@ class Inspector:
 if __name__ == '__main__':
     inspector = Inspector()
     inspector.show_params()
+    print(inspector.export_to_dict())
---
+++ testing/tests/test_inspector.py
@@ -0,0 +1,20 @@
+"""
+Тест-набор для тестирования функций модуля "inspector".
+"""
+import allure
+from app.modules.inspector import Inspector
+
+inspector = Inspector()
+
+@allure.title("Проверка работы модуля;")
+class TestInspector:
+    
+    def test_returned_params():
+        """
+        Тест, проверяющий количество собранных параметров.
+        """
+        system_params = inspector.export_to_dict()
+        assert isinstance(system_params, dict), \
+            f"Error - wrong returned type!\n \
+            Expected - dict, returned - {type(system_params)}"
+    
